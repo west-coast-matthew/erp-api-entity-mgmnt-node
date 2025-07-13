4fddaf11530989a9cfdd15848f9bab0961132522
Matthew Dalby <matthew@matthewdalby.dev>
2025-07-13 09:54:29 -0700

    datasource works

--- app/src/app.ts
+++ app/src/app.ts
@@ -1,12 +1,15 @@
 //import 'module-alias/register';
-import express, { ErrorRequestHandler, Express } from 'express';
+//import express, { ErrorRequestHandler, Express } from 'express';
+import express  from 'express';
 import helmet from 'helmet';
-import router from '@/routes/index.route';
-import {createOrmConnection} from '@west-coast-matthew/erp-core-node';
+//import router from '@routes/index.route';
+import {createDataSource} from '@west-coast-matthew/erp-core-node';
 import {errorHandlerMiddleware} from '@west-coast-matthew/erp-core-node';
 import {InitializationException} from '@west-coast-matthew/erp-core-node';
 import dotenv from 'dotenv';
-const _ = require('lodash');
+import _  from 'lodash';
+
+import "reflect-metadata";
 
 // todo [ERP-16]: enable security
 //import securityMiddleware from './middleware/security.middleware';
@@ -40,10 +43,10 @@ const reqEnvProps = ['DB_HOST','DB_PORT','DB_USERNAME','DB_PASSWORD'];
 
 validatePropsPresent(reqEnvProps);
 
-const dataSource = createOrmConnection({
+const dataSource = createDataSource({
   type: 'mariadb',
   host: process.env.DB_HOST || '',
-  port: process.env.DB_PORT? Number(process.env.DB_PORT) ||  -1,
+  port: process.env.DB_PORT? Number(process.env.DB_PORT) :  -1,
   username: process.env.DB_USERNAME || '',
   password: process.env.DB_PASSWORD || '',
   database: 'erp',
@@ -68,14 +71,14 @@ app.use(helmet());
 app.use(express.json());
 // todo [ERP-16]: enable security
 //app.use(securityMiddleware);
-app.use('/', router);
+//app.use('/', router);
 
-/*
-app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-  res.status(500).json({ message: err.message });
-});*/
 
-app.use(errorHandlerMiddleware);
+//app.use((_err: Error, _req: Request, _res: Response, _next: NextFunction) => {
+//  res.status(500).json({ message: err.message });
+//});
+
+//app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5176;
 
